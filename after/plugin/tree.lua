93d2036f0c891095790049c39824c5ddc2072572
MaxwelMazur <maxwelbm@hotmail.com>
2023-02-28 13:03:47 -0300

    criando uma status line simples e funcional

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -14,23 +14,55 @@ end
 
 -- OR setup with some options
 tree.setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
+    sort_by = "case_sensitive",
+    view = {
+        -- Opções de exibição
+        width = 30,
+        mappings = {
+            list = {
+                { key = "u", action = "dir_up" },
+            },
+        },
+        -- defina o campo devicons para personalizar os ícones exibidos
+        devicons = {
+            -- defina um ícone personalizado para a extensão .md
+            default = "",
+            symlink = "",
+            git = {
+                -- defina ícones personalizados para os diferentes status do git
+                unstaged = "✚",
+                staged = "✚",
+                unmerged = "≠",
+                renamed = "≫",
+                deleted = "",
+                ignored = "◌",
+            },
+            folder = {
+                arrow_open = "",
+                arrow_closed = "",
+                default = "",
+                open = "",
+                empty = "",
+                empty_open = "",
+                symlink = "",
+                symlink_open = "",
+            },
+            lsp = {
+                hint = "",
+                info = "",
+                warning = "",
+                error = "",
+            },
+
+        },
     },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = false,
+    }
 })
 
 vim.keymap.set("n", "<c-n>", ":NvimTreeToggle<CR>")
-
 vim.opt.laststatus = 3
