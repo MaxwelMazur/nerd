e8c50243e3d40e6c42deee50545f789b877cff94
MaxwelMazur <maxwelbm@hotmail.com>
2023-03-08 07:47:53 -0300

    ref: indent and changed color

--- after/plugin/alpha.lua
+++ after/plugin/alpha.lua
@@ -37,22 +37,22 @@ local options = {
   header = {
     type = "text",
     val = {
-      "                                            88",
-      "                                            88",
-      "                                            88",
-      " 8b,dPPYba,   ,adPPYba, 8b,dPPYba,  ,adPPYb,88",
-      " 88P'   `\"8a a8P_____88 88P'   \"Y8 a8\"    `Y88",
-      " 88       88 8PP\"\"\"\"\"\"\" 88         8b       88",
-      " 88       88 \"8b,   ,aa 88         \"8a,   ,d88",
-      " 88       88  `\"Ybbd8\"' 88          `\"8bbdP\"Y8",
+        "                                            88",
+        "                                            88",
+        "                                            88",
+        " 8b,dPPYba,   ,adPPYba, 8b,dPPYba,  ,adPPYb,88",
+        " 88P'   `\"8a a8P_____88 88P'   \"Y8 a8\"    `Y88",
+        " 88       88 8PP\"\"\"\"\"\"\" 88         8b       88",
+        " 88       88 \"8b,   ,aa 88         \"8a,   ,d88",
+        " 88       88  `\"Ybbd8\"' 88          `\"8bbdP\"Y8",
     },
     opts = {
-      position = "center",
-      hl = "AlphaHeader",
+        position = "center",
+        hl = "AlphaHeader",
     },
-  },
+},
 
-  buttons = {
+buttons = {
     type = "group",
     val = {
       button("SPC f f", "  Find File  "),
@@ -77,3 +77,6 @@ alpha.setup({
   },
   opts = {},
 })
+
+-- Definindo as cores do cabeçalho usando highlights
+vim.cmd([[ hi AlphaHeader guifg=#3CB371 guibg=none ]])
