a1a1062fde870b17aada45e146181fa21a5db048
MaxwelMazur <maxwelbm@hotmail.com>
2023-03-08 07:48:54 -0300

    ref: adjust bindkeys and style new file with gitsigns

--- lua/bindkeys.lua
+++ lua/bindkeys.lua
@@ -7,9 +7,7 @@ keymap("v", "K", ":m '<-2<CR>gv=gv")
 keymap("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/cgi<Left><Left><Left>]])
 keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 keymap("n", "<leader>q", ":q!<CR>")
-
-keymap("n", "<C-d>", "<cmd>t.<CR>")
--- keymap("n", "<leader>d", "<cmd>t.<CR>")
+keymap("n", "<leader>d", "<cmd>t.<CR>")
 keymap("n", "<leader>wh", "<cmd>split<CR>")
 keymap("n", "<leader>wv", "<cmd>vsplit<CR>")
 
@@ -20,19 +18,19 @@ keymap("n", "<C-k>", ":resize -3<CR>")
 
 keymap('n', '<leader>cw', [[:let @/='\<'.expand('<cword>').'\>'<cr>"_ciw]], { noremap = true })
 
--- Auto-indent and save on Ctrl+s
 keymap('n', '<C-s>', ':w<CR>gg=G<C-o>')
 keymap('n', '<leader>s', ':w<CR>gg=G<C-o>')
 keymap("n", "<C-a>", "ggVG")
 keymap("n", "<leader>a", "ggVG")
 
-keymap('n', '<C-j>', '<cmd>split term://bash | resize 18 <CR>', { noremap = true, silent = true, nowait = true })
+keymap('n', '<leader>t', '<cmd>split term://zsh | resize 18 <CR>', { noremap = true, silent = true, nowait = true })
 
 -- function _G.set_terminal_keymaps()
---     local opts = {buffer = 0}
---     vim.keymap.set('t', '<C-w>h', [[<Cmd>wincmd h<CR>]], opts)
---     vim.keymap.set('t', '<C-w>j', [[<Cmd>wincmd j<CR>]], opts)
---     vim.keymap.set('t', '<C-w>k', [[<Cmd>wincmd k<CR>]], opts)
---     vim.keymap.set('t', '<C-w>l', [[<Cmd>wincmd l<CR>]], opts)
---     vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
--- end
+    --     local opts = {buffer = 0}
+    --     vim.keymap.set('t', '<C-w>h', [[<Cmd>wincmd h<CR>]], opts)
+    --     vim.keymap.set('t', '<C-w>j', [[<Cmd>wincmd j<CR>]], opts)
+    --     vim.keymap.set('t', '<C-w>k', [[<Cmd>wincmd k<CR>]], opts)
+    --     vim.keymap.set('t', '<C-w>l', [[<Cmd>wincmd l<CR>]], opts)
+    --     vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    -- end
+    --   
